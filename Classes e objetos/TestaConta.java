11fd7373d79106423a613e2092682547e203b26a
Cassiano Borba <bcassiano888@gmail.com>
2026-02-26 16:01:51 -0300

    Update TestaConta.java

--- Classes e objetos/TestaConta.java
+++ Classes e objetos/TestaConta.java
@@ -1,13 +1,14 @@
 public class Main{
     public static void main(String[] args) {
     
-    Conta c1 = new Conta(123,1,"cassiano");
+    Conta c1 = new Conta(123,1,"João Silva");
+    c1.depositar(500);
+    c1.sacar(200);
     System.out.println(c1.toString());
-    c1.depositar(100);
-    c1.sacar(50);
-    c1.sacar(70);
+    Conta c2 = new Conta(1234,1,"Maria Souza");
+    c2.sacar(50);
     c1.encerrarConta();
-    c1.sacar(50);
+    c1.sacar(300);
     c1.encerrarConta();
     System.out.println(c1.toString());
 
