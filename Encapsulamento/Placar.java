59e0cedacdb98441f397691e7534e53d662759b7
Cassiano Borba <bcassiano888@gmail.com>
2026-03-08 22:23:33 -0300

    Update Placar.java

--- Encapsulamento/Placar.java
+++ Encapsulamento/Placar.java
@@ -1,27 +1,82 @@
-public class Placar{
+public class Placar {
     private String casaNome, visitanteNome;
     private int pontosCasa, pontosVisitante, perQuarto;
-        
-    public Placar(String casa, String visitante){
+
+    public Placar(String casa, String visitante) {
         this.pontosCasa = 0;
-        this.pontosVisitante = 0
+        this.pontosVisitante = 0;
         this.perQuarto = 1;
-        this.setNomes(casa,visitante)
-    
+        this.setNomeCasa(casa);
+        this.setNomeVisitante(visitante);
+
     }
-    public void setNomes(String casa, String visitante){
+
+    public void setNomeCasa(String casa) {
         this.casaNome = casa;
-        this.visitanteNome =  visitante;
     }
-    public void registrarpt(String time,int pt){
-       if(pt > 0 %% pt <= 3){
-            if(time == "casa"){
-                this.pontosCasa += pt;
-            }
-            else if( time == "visitante"){
-                this.pontosVisitante += pt;
+
+    public void setNomeVisitante(String visitante) {
+        this.visitanteNome = visitante;
+    }
+
+    public void registrarpt(String time, int pt) {
+        if (time == "casa") {
+            switch (pt) {
+                case 1:
+                    this.pontosCasa += 1;
+                    System.out.println("lance livre convertido");
+                    break;
+                case 2:
+                    this.pontosCasa += 2;
+                    System.out.println("cesta de 2 convertida.");
+                    break;
+                case 3:
+                    this.pontosCasa += 3;
+                    System.out.println("cesta de 3 convertida.");
+                    break;
+                default:
+                    System.out.println("valor inválido.");
+                    break;
             }
         }
+        else if(time == "visitante"){
+            switch (pt){
+                case 1:
+                    this.pontosVisitante += 1;
+                    System.out.println("lance livre convertido");
+                    break;
+                case 2: this.pontosVisitante += 2;
+                    System.out.println("cesta de 2 convertida.");
+                    break;
+                case 3: this.pontosVisitante +=3;
+                    System.out.println("cesta de 3 convertida.");
+                    break;
+                default:
+                    System.out.println("valor inválido.");
+                    break;
+            }
+        }
+        else{
+            System.out.println("time inválido.");
+        }
     }
-    
+    public void proxTemp() {
+        if (this.perQuarto == 4) {
+            System.out.println("fim do " + this.perQuarto+" tempo");
+            System.out.println("Jogo acabou.");
+        } else {
+            System.out.println("fim do " + this.perQuarto+" tempo");
+            this.perQuarto += 1;
+        }
+    }
+
+    public String toString(){
+        return "TIME CASA: " + this.casaNome
+                + " Pontos: "+ this.pontosCasa
+                + " x "
+                + "TIME VISITANTE " + this.visitanteNome
+                + " Pontos: " + this.pontosVisitante
+                + " Período: " + this.perQuarto;
+    }
+
 }
