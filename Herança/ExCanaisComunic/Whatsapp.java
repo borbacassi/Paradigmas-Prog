4ee86c5eb190fc7b0c889d769c958410ad2d08ca
Cassiano Borba <bcassiano888@gmail.com>
2026-03-31 20:31:09 -0300

    Create Whatsapp.java

---
+++ Herança/ExCanaisComunic/Whatsapp.java
@@ -0,0 +1,24 @@
+public class Whatsapp extends CanalNotificacao{
+    private String statusLeitura;
+
+    public Whatsapp(){
+        super();
+    }
+    public Whatsapp(String destinatario, String mensagem, String statusLeitura){
+        super(destinatario,mensagem);
+        this.statusLeitura = statusLeitura;
+    }
+
+    public String getStatusLeitura() {
+        return statusLeitura;
+    }
+
+    public void setStatusLeitura(String statusLeitura) {
+        this.statusLeitura = statusLeitura;
+    }
+
+    public void enviar(){
+        System.out.println("Enviando Zap para "
+                + this.destinatario + ".... " + "Mensagem: "+ this.mensagem);
+    }
+}
